3641778c9540980896e9408b642e4e635732bfa4
hodanghuy <hodanghuy977@gmail.com>
2024-04-24 11:45:19 +0700

    set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import MainLayout from './components/MainLayout';
 import NotFound from './components/NotFound';
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainLayout />}></Route>
+        <Route path="/" element={<Login />}></Route>
       
         <Route path="*" element={<NotFound />} />
       </Routes>
