4351881deca38886f5d638d0d36393b2ad1bb0e0
hodanghuy <hodanghuy977@gmail.com>
2024-06-11 15:58:07 +0700

    [fix] sửa trang thông báo thanh toán

--- frontend/src/pages/Cart/PaymentFail.js
+++ frontend/src/pages/Cart/PaymentFail.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const PaymentFail = () => {
   return (
-    <div>PaymentFail</div>
+    <>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{
+          height: '100vh',
+          backgroundColor: '#b3e6ff',
+        }}
+      >
+        <div className="text-danger display-4">
+          <AiFillCloseCircle className="mr-3" />
+          Thanh toán thất bại
+        </div>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/Cart/PaymentSuccess.js
+++ frontend/src/pages/Cart/PaymentSuccess.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { FaCheckCircle } from 'react-icons/fa'
 
 const PaymentSuccess = () => {
   return (
-    <div>PaymentSuccess</div>
+    <>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{
+          height: '100vh',
+          backgroundColor: '#b3e6ff',
+        }}
+      >
+        <div className="text-success display-4">
+          <FaCheckCircle className="mr-3" />
+          Thanh toán thành công
+        </div>
+      </div>
+    </>
   )
 }
 
